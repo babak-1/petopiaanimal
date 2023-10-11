53d2dddfe8e3db572edc5d2a7069b8b3d200a089
babak-1 <babek.taghiyev@gmail.com>
2023-10-11 20:28:15 +0400

    demo create announce

--- src/pages/CreateAnnounce/CreateAnnounce.jsx
+++ src/pages/CreateAnnounce/CreateAnnounce.jsx
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./CreateAnnounce.module.scss";
 import { useForm } from "react-hook-form";
 import useFetch from "../../hooks/useFetch";
 
 const CreateAnnounce = () => {
-  const allCategories = useFetch("/api/categories/getcategorieswithbreeds");
+  // usestate
+  const [selectCategory, setSelectCategory] = useState(null);
+  const [selectBreed, setSelectBreed] = useState(null);
+  const [selectCity, setSelectCity] = useState(null);
+  const [breeds, setBreeds] = useState([]);
+
+  // fetch
+  const allCategories = useFetch(
+    "/api/categories/getcategorieswithbreeds"
+  ).response;
+
+  const allCities = useFetch("/api/cities").response;
+
+  useEffect(() => {
+    if (selectCategory) {
+      const selectedCategory = allCategories.find(
+        (category) => category.id == selectCategory
+      );
+      if (selectedCategory) {
+        setBreeds(selectedCategory.breeds);
+      }
+    } else {
+      setBreeds([]);
+    }
+  }, [selectCategory, allCategories]);
 
   const {
     handleSubmit,
@@ -17,6 +41,11 @@ const CreateAnnounce = () => {
     console.log(data);
     reset();
   };
+
+  // console.log
+  console.log("active kateqori id", selectCategory);
+  console.log("active alt kateqori id", selectBreed);
+  console.log("active seher", selectCity);
 
   return (
     <div className={style.container}>
@@ -39,10 +68,46 @@ const CreateAnnounce = () => {
               placeholder="Vacib meqamlari qeyd et"
             />
           </li>
-          <li></li>
+          <li>
+            <select
+              name="breeds"
+              id="breeds"
+              onChange={(e) => setSelectCategory(e.target.value)}
+            >
+              {allCategories?.map((category) => (
+                <option key={category?.id} value={category?.id}>
+                  {category?.name}
+                </option>
+              ))}
+            </select>
+          </li>
 
-          <li></li>
-          <li></li>
+          <li>
+            <select
+              name="breeds"
+              id="breeds"
+              onChange={(e) => setSelectBreed(e.target.value)}
+            >
+              {breeds.map((breed) => (
+                <option key={breed.id} value={breed.id}>
+                  {breed.name}
+                </option>
+              ))}
+            </select>
+          </li>
+          <li>
+            <select
+              name="cities"
+              id="cities"
+              onChange={(e) => setSelectCity(e.target.value)}
+            >
+              {allCities?.map((city) => (
+                <option key={city?.id} value={city?.id}>
+                  {city?.name}
+                </option>
+              ))}
+            </select>
+          </li>
           <li></li>
         </ul>
         <button onClick={handleSubmit(onSubmit)}>Davam et</button>
