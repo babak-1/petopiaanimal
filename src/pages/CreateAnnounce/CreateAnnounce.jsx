3dc8f1595c098afaa21899f0c99aba5494b8b291
babak-1 <babek.taghiyev@gmail.com>
2023-10-14 14:30:20 +0400

    merge

--- src/pages/CreateAnnounce/CreateAnnounce.jsx
+++ src/pages/CreateAnnounce/CreateAnnounce.jsx
@@ -32,20 +32,15 @@ const CreateAnnounce = () => {
   };
 
   if (loading) {
-<<<<<<< HEAD
     return (
       <div className={style.loadingCont}>
         <ClockLoader color="#36d7b7" />
       </div>
     );
-=======
-    return <h3>loading...</h3>;
->>>>>>> 160cb209839857c889c729b1428d0d3c87133728
   }
 
   return (
     <div className={style.container}>
-<<<<<<< HEAD
       <h3 className={style.headding}>Create Announce</h3>
       <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
         <input
@@ -98,56 +93,6 @@ const CreateAnnounce = () => {
         <button type="submit" className={style.btn}>
           Davam et
         </button>
-=======
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h3>Create Announce</h3>
-        <div
-          style={{
-            display: "inline-flex",
-            flexDirection: "column",
-            gap: "16px",
-            padding: "16px",
-          }}
-        >
-          <input type="text" {...register("name")} placeholder="name" />
-          <input
-            type="text"
-            className={`${style.contentInput}`}
-            placeholder="Vacib meqamlari qeyd et"
-            {...register("content")}
-          />
-          <select
-            {...register("categoryId")}
-            onChange={(e) => setCategoryId(e.target.value)}
-          >
-            {categories.map((cat) => (
-              <option key={cat.id} value={cat.id}>
-                {cat.name}
-              </option>
-            ))}
-          </select>
-          <select
-            {...register("breedId")}
-            onChange={(e) => setValue("breedId", e.target.value)}
-          >
-            {categories
-              .find((cat) => cat.id === parseInt(categoryId))
-              .breeds.map((breed) => (
-                <option key={breed.id} value={breed.id}>
-                  {breed.name}
-                </option>
-              ))}
-          </select>
-          <select {...register("cityId")}>
-            {allCities.map((city) => (
-              <option key={city.id} value={city.id}>
-                {city.name}
-              </option>
-            ))}
-          </select>
-          <button type="submit">Davam et</button>
-        </div>
->>>>>>> 160cb209839857c889c729b1428d0d3c87133728
       </form>
     </div>
   );
